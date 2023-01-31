5aff7a302dec7ad6ad45ca055fc825c2147f3249
Daniel Marinado <daniel.marinado@gmail.com>
2023-01-31 15:52:49 -0300

    Agrega login(auth), y homePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-fajyxgnfrvohzzxu.us.auth0.com',
+      clientId: 'wLV8MD8WGiIjxm5s9Yytyev0ekSdtU0l',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
